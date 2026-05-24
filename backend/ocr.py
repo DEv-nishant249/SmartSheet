a6b4676344f1695be71333929b3876cec1a1cb30
developer_Nishant <developernishant16@gmail.com>
2026-05-24 15:23:37 +0530

    optimized

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,11 +1,42 @@
 import easyocr
+import gc
 
-reader = easyocr.Reader(['en'], gpu=False)
+reader = None
 
-def extract_text_from_image(image_path: str):
-    results = reader.readtext(image_path)
 
-    if not results:
-        return ""
+def get_reader():
+    global reader
 
-    return "\n".join([r[1] for r in results])
+    if reader is None:
+        print("Initializing EasyOCR...")
+
+        reader = easyocr.Reader(
+            ['en'],
+            gpu=False,
+            verbose=False
+        )
+
+        print("EasyOCR Ready")
+
+    return reader
+
+
+def extract_text_from_image(image_path):
+    try:
+        ocr_reader = get_reader()
+
+        results = ocr_reader.readtext(
+            image_path,
+            detail=0,
+            paragraph=True
+        )
+
+        text = "\n".join(results)
+
+        gc.collect()
+
+        return text
+
+    except Exception as e:
+        gc.collect()
+        raise RuntimeError(str(e))
